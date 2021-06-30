0bcd322abcd4b99702adfd0e8705674599fdf092
duahau7412 <74884632+duahau7412@users.noreply.github.com>
2021-07-01 04:33:38 +0700

    Update Terminal.h

--- Terminal.h
+++ Terminal.h
@@ -6,6 +6,7 @@ using namespace std;
 bool isNumber(string s);
 bool isValidAlgorithm(string algorithm, int& valid_algorithm);
 bool isValidOutputParam(string output_param, int& valid_output);
+bool isValidDataOrder(string data_order, int& valid_order);
 bool handleAlgorithmsMode(int argc, char* argv[]);
 bool handleComparisonMode(int argc, char* argv[]);
 bool handleArguments(int argc, char* argv[]);
